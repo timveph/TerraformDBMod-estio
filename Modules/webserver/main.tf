0b3e5d551242c55f75fd7b009a911fee8c2e1af1
nathanforester <nfeugene86@gmail.com>
2022-06-15 16:03:03 +0100

    execute script manually

--- Modules/webserver/main.tf
+++ Modules/webserver/main.tf
@@ -27,24 +27,20 @@ resource "null_resource" "connect_web" {
 }
 
 resource "null_resource" "cluster" {
-  # Changes to any instance of the cluster requires re-provisioning
-  triggers = {
-    cluster_instance_ids = join(",", aws_instance.cluster.*.id)
-  }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
+
   connection {
-    host = element(aws_instance.cluster.*.public_ip, 0)
-  }
+      host        = aws_instance.webserver.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("./myKey")
+    }
 
   provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the clutser
     inline = [
       "sudo apt update",
       "sudo apt install mysql-server -y",
-      "git clone https://github.com/nathanforester/FlaskMovieDB2.git",
-      "./home/ubuntu/FlaskMovieDB2/startup.sh"
+      "git clone https://github.com/nathanforester/FlaskMovieDB2.git"
     ]
   }
 }
